8e5c76f348e27b16db63f92ba2952313b6be670e
Wataru MIYAGUNI <gonngo@gmail.com>
2014-08-07 20:37:02 +0900

    Test first writing !!

---
+++ client_test.go
@@ -0,0 +1,86 @@
+package airplay
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+type testHundelrFunc func(*testing.T, http.ResponseWriter, *http.Request)
+
+func TestStop(t *testing.T) {
+	ts := airTestServer(t, "POST", "/stop", nil)
+	client := getTestClient(t, ts)
+	client.Stop()
+}
+
+func TestScrub(t *testing.T) {
+	position := 12.345
+	ts := airTestServer(t, "POST", "/scrub", func(t *testing.T, w http.ResponseWriter, req *http.Request) {
+		values := req.URL.Query()
+		positionString := values.Get("position")
+		if positionString == "" {
+			t.Error("Not found query parameter `position`")
+		}
+
+		positionFloat, err := strconv.ParseFloat(positionString, 64)
+		if err != nil {
+			t.Error("Incorrect query parameter `position` (actual = %s)", positionString)
+			return
+		}
+
+		if positionFloat != position {
+			t.Errorf("Incorrect query parameter `position` (actual = %f)", positionFloat)
+			return
+		}
+	})
+	client := getTestClient(t, ts)
+	client.Scrub(float32(position))
+}
+
+func airTestServer(t *testing.T, expectMethod, expectPath string, handler testHundelrFunc) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != expectMethod || req.URL.Path != expectPath {
+			t.Errorf("request is not '%s %s' (actual = %s %s)",
+				expectMethod, expectPath, req.Method, req.URL.Path)
+			return
+		}
+
+		if handler != nil {
+			handler(t, w, req)
+		}
+	}))
+}
+
+func getTestClient(t *testing.T, ts *httptest.Server) *Client {
+	addr, port := getAddrAndPort(t, ts.URL)
+	device := Device{
+		Addr: addr,
+		Port: port,
+	}
+	client, err := NewClientHasDevice(device)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return client
+}
+
+func getAddrAndPort(t *testing.T, host string) (string, int) {
+	u, err := url.Parse(host)
+	if err != nil {
+		t.Error(err)
+	}
+
+	split := strings.Split(u.Host, ":")
+	addr := split[0]
+	port, err := strconv.Atoi(split[1])
+	if err != nil {
+		t.Error(err)
+	}
+
+	return addr, port
+}
