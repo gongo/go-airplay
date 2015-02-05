df9267b39eaf5e0649fc670dacd41d73178a3c64
Wataru MIYAGUNI <gonngo@gmail.com>
2015-02-05 21:49:38 +0900

    Add FirstDevice()

--- device.go
+++ device.go
@@ -14,13 +14,28 @@ func Devices() []Device {
 	for _, entry := range searchEntry(&queryParam{}) {
 		devices = append(
 			devices,
-			Device{
-				Name: entry.hostName,
-				Addr: entry.ipv4.String(),
-				Port: int(entry.port),
-			},
+			entryToDevice(entry),
 		)
 	}
 
 	return devices
+}
+
+// FirstDevice return the first AirPlay device in LAN it is found.
+func FirstDevice() Device {
+	params := &queryParam{maxCount: 1}
+
+	for _, entry := range searchEntry(params) {
+		return entryToDevice(entry)
+	}
+
+	return Device{}
+}
+
+func entryToDevice(entry *entry) Device {
+	return Device{
+		Name: entry.hostName,
+		Addr: entry.ipv4.String(),
+		Port: int(entry.port),
+	}
 }
